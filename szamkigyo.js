6a085bd3fb73fe76f1d7250598ad8ee30eabc317
swisskanton <kormanyos.antal@gmail.com>
2018-10-24 21:33:49 +0200

    kezdő beállítások megoldva, probléma a viewControlTable-nél 42. sor, végtelen cikluba megy

--- szamkigyo.js
+++ szamkigyo.js
@@ -11,12 +11,12 @@ const makeTable = (diff, level) => {
         line = [];
         for (let j = 0; j < diff; j++) {
             line.push(' ');
-        }
+        };
         userTable.push(line);
         //controlTable.push(line);
-    }
+    };
     return userTable;
-}
+};
 
 const setControlTable = (controlTable, diff, level) => {
     let x, y, i = 0;
@@ -26,16 +26,28 @@ const setControlTable = (controlTable, diff, level) => {
         if (controlTable[x][y] == ' ') {
             i++;
             controlTable[x][y] = i;
-        }
-    }
+        };
+    };
     //getTable(controlTable);
-}
+};
 
 const getTable = (matrix) => {
     output = table.table(matrix);
     clear(true);
     console.log(output);
-}
+};
+
+const viewControlTable = (matrix) => {
+    getTable(matrix);
+    let go = readlineSync.question('Do you want to start this level? [y/n]: '); 
+    if (go == 'y' || 'Y') {
+        console.log('igaz');
+        return true;
+    } else {
+        console.log('hamis');
+        return false;
+    };
+};
 
 const getMove = (matrix, diff, level, winner) => {
     let posX = 0, posY = 0;
@@ -45,16 +57,16 @@ const getMove = (matrix, diff, level, winner) => {
     let guess = [];
     for (let i=1; i <= level; i++) {
         guess.push(i);
-    }
+    };
     do {
         getTable(matrix);
         keypress(process.stdin);
-        process.stdin.setRawMode(true);
+        process.stdin.setRawMode(false);
         process.stdin.resume();
         process.stdin.on('keypress', function (ch, key) {
             if (key && key.ctrl && key.name == 'c') {
                 process.stdin.pause();
-            }
+            };
             console.log(key.name);
             switch (key.name) {
                 case 'up':
@@ -100,40 +112,73 @@ const getMove = (matrix, diff, level, winner) => {
                         guess.sort();
                         backInfo = ' ';
                         console.log('n', backInfo, guess);
-                    }
-                    
+                    };
+            
                     if (guess.length == 0) {
-                        process.stdin.setRawMode(false);
-                        return isEnd(matrix, winner);
-                    }
+                        //process.stdin.setRawMode(false);
+                        if (isCorrect(matrix, winner)) {
+                            process.stdin.setRawMode(false);
+                            return true;
+                        }
+                    };
                     
                     break;
                 default:
-                    console.log('téves karakter');
+                    console.log('wrong character');
                     break;
             };
         });
 
     } while (ki == 1);
     process.stdin.resume();
-}
-const isEnd = (matrix, winner) => {
+};
+const isCorrect = (matrix, winner) => {
         for(var i = matrix.length; i--;) {
             if(matrix[i] !== winner[i]) {
-                return 'Sajnálom, nem nyertél.';
-            }
-        }
-    
-        return 'Gratulálok nyertél!';
-}
-let difficulty = 5;
+                return false;
+            };
+        };
+        return true;
+};
+
+let difficulty = 7;
 let level = 3;
-difficulty = readlineSync.question("Milyen nehézségi szinten játszol (3, 5, 7)?");
-level = readlineSync.question("Hány számmal szeretnél kezdeni (3-12)?");
-let gameTable = makeTable(difficulty, level);
-let winnTable = makeTable(difficulty, level);
-setControlTable(winnTable, difficulty, level);
-end = getMove(gameTable, difficulty, level, winnTable);
-console.log(end);
-//getTable(viewTable);
-//getMove(gameTable);
+let options = ['beginner', 'advanced', 'master'];
+difficulty = readlineSync.keyInSelect(options, "Please, select the difficulty level?");
+switch (difficulty) {
+    case 0 :
+        difficulty = 3;
+        break;
+    case 1 :
+        difficulty = 5;
+        break;
+    case 2 :
+        difficulty = 7;
+        break;
+    case -1 :
+        console.log('Thank you for visiting.');
+};
+if (difficulty != -1) {
+    level = readlineSync.question("With how many numbers woud you like to start? [3-12]: ");
+    let endOfGame = 1;
+    while (endOfGame == 1) {
+        let gameTable = makeTable(difficulty, level);
+        let winnTable = makeTable(difficulty, level);
+        setControlTable(winnTable, difficulty, level);
+        let toGame = viewControlTable(winnTable);
+        console.log('toGame: ', toGame);
+        if (toGame == true) {
+           if (getMove(gameTable, difficulty, level, winnTable)) {
+                console.log('Congratulation! You win!');
+                if (readlineSync.keyInYN('Woud you like to play on the next level?')) {
+                    level++;  
+                } else {
+                    endOfGame = 0;
+                    console.log('Thank you for a game.');
+                };
+           };
+        } else {
+            console.log('Thank you for watching.');
+        };
+    };
+};
